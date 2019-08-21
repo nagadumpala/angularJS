45a7f2810ae9856bb2929b66242bfd9ad038e2d1
Naga Dumpala <dumpalan@gmail.com>
2019-08-21 15:27:38 +0530

    f

--- step1-HelloWorld/src/app/naga.component.ts
+++ step1-HelloWorld/src/app/naga.component.ts
@@ -3,9 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'naga-root',
   template: `
-  <h1>{{title}}</h1>
-  `,
+  <div> 
+       <h1 [innerHTML]="title"> </h1>   
+    <button (click)="clickHAndler()">Clcik ME</button>   
+    <hr> 
+    <input [value]="title" type="text">
+    <hr> 
+    <input #til type="text">
+    <hr> 
+    <button (click)="checkValueHandler(til.value)">Check value</button>   
+  <img [src]="photo">
+  <img bind-src="photo">
+  <img src={{photo}}>
+    </div>
+   
+    `         
 })
-export class NagaComponent {
+export class NagaComponent {         
   title = 'HelloWorld !!';
-}
+  photo = 'assets/images/rajani.jpg   ';
+  clickHAndler(){
+    alert('Heallo');
+  }   
+
+  checkValueHandler(mesg:string){
+    alert('Heallo  '+mesg);
+  }   
+} 
